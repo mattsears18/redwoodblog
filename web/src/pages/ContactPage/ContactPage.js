01e1e329821ca25ce66309c6e7018bbf8ddb8074
Matt Sears <mattsears18@gmail.com>
2022-02-13 11:22:36 -0500

    save

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,13 +1,33 @@
 import { MetaTags } from '@redwoodjs/web'
-import { Form, TextField, Submit } from '@redwoodjs/forms'
+import {
+  FieldError,
+  Form,
+  TextField,
+  TextAreaField,
+  Submit,
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form>
-        <TextField name="input" />
+      <Form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <TextField name="name" validation={{ required: true }} />
+        <FieldError name="name" className="error" />
+
+        <label htmlFor="email">Email</label>
+        <TextField name="email" validation={{ required: true }} />
+        <FieldError name="email" className="error" />
+
+        <label htmlFor="message">Message</label>
+        <TextAreaField name="message" validation={{ required: true }} />
+        <FieldError name="message" className="error" />
 
         <Submit>Save</Submit>
       </Form>
