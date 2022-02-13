d08efd1c06f838e263b2553b202bca22cb6323d6
Matt Sears <mattsears18@gmail.com>
2022-02-13 13:10:48 -0500

    article cells and pages

---
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -0,0 +1,28 @@
+// web/src/components/ArticlesCell/ArticlesCell.js
+
+import Article from 'src/components/Article'
+
+export const QUERY = gql`
+  query ArticlesQuery {
+    articles: posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error.message}</div>
+)
+
+export const Success = ({ articles }) => {
+  return articles.map((article) => (
+    <Article key={article.id} article={article} />
+  ))
+}
